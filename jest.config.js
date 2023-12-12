836dd582952b160e8bf7a18b823198c8932c557f
mustaphaibnel <mustaphaibnel@gmail.com>
2023-12-12 21:08:38 +0100

    add coverage test

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+// jest.config.js
+module.exports = {
+    testEnvironment: 'node', // This is important for running tests in Node.js environment
+    collectCoverage: true,
+    coverageReporters: ['html', 'text-summary'],
+  };
+  
