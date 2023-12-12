401d3c1d53c698cf02c80904351b649d77b15fa4
mustaphaibnel <mustaphaibnel@gmail.com>
2023-12-12 20:53:30 +0100

    add src structure latest

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
 
 describe('Calculator API', () => {
   test('adds two numbers', async () => {
