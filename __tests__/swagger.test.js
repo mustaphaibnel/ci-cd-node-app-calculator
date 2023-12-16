1988ae70fbf2ec26555c27da9e52c13d66ffb7cb
mustaphaibnel <mustapha.ibnel@gmail.com>
2023-12-16 06:43:36 +0100

    the new api testing

--- __tests__/swagger.test.js
+++ __tests__/swagger.test.js
@@ -7,26 +7,24 @@ describe('Swagger Documentation Setup', () => {
 
   beforeEach(() => {
     app = express();
-    swaggerSetup(app); // Apply the Swagger setup to the Express app instance
+    swaggerSetup(app);
   });
 
   it('should serve the Swagger UI at /api-docs/', async () => {
-    // Adjusted to handle potential redirection to /api-docs/
     const response = await request(app).get('/api-docs/');
     expect(response.statusCode).toBe(200);
     expect(response.text).toContain('Swagger UI');
   });
 
   it('should have the correct Swagger JSON document', async () => {
-    // Adjusted to handle potential redirection and correct endpoint for Swagger JSON
     const response = await request(app).get('/api-docs/swagger.json').redirects(1);
-    expect(response.statusCode).toBe(200); // Ensure a 200 response
+    expect(response.statusCode).toBe(200);
 
     try {
       const swaggerDoc = JSON.parse(response.text);
-      // You can add more checks here if necessary
+      // Additional assertions can be added here if needed
     } catch (error) {
-      throw new Error(`Failed to parse Swagger JSON: ${error}`);
+      throw new Error(`Failed to parse Swagger JSON: ${error.message}`);
     }
   });
 });
