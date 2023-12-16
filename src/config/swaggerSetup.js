34e53fb1621ca44c081f67713dc5922459364288
mustaphaibnel <mustapha.ibnel@gmail.com>
2023-12-16 06:30:00 +0100

    the new api testing

--- src/config/swaggerSetup.js
+++ src/config/swaggerSetup.js
@@ -1,19 +1,43 @@
-const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const jsYaml = require('js-yaml');
+const request = require('supertest');
+const express = require('express');
+const swaggerSetup = require('../src/config/swaggerSetup');
 
-const swaggerSetup = (app) => {
-    const swaggerYAMLPath = './src/swagger.yaml';
-    const swaggerDocument = jsYaml.load(fs.readFileSync(swaggerYAMLPath, 'utf8'));
+describe('Swagger Documentation Setup', () => {
+  let app;
+  let server;
+  let agent;
 
-    app.use('/api-docs', (req, res, next) => {
-        const swaggerDocCopy = JSON.parse(JSON.stringify(swaggerDocument));
-        swaggerDocCopy.host = req.get('host');
-        req.swaggerDoc = swaggerDocCopy;
-        next();
-    }, swaggerUi.serve, (req, res) => {
-        swaggerUi.setup(req.swaggerDoc)(req, res);
+  beforeAll((done) => {
+    app = express();
+    swaggerSetup(app);
+    server = app.listen(0, () => {
+      agent = request.agent(server); // Create an agent to handle requests
+      done();
     });
-};
+  });
 
-module.exports = swaggerSetup;
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  it('should serve the Swagger UI at /api-docs/', async () => {
+    const response = await agent.get('/api-docs/');
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain('Swagger UI');
+  });
+
+  it('should have the correct host in the Swagger document', async () => {
+    const response = await agent.get('/api-docs/swagger.json').redirects(1);
+    
+    let swaggerDoc;
+    try {
+      swaggerDoc = JSON.parse(response.text);
+    } catch (error) {
+      throw new Error(`Failed to parse Swagger JSON: ${error}`);
+    }
+
+    // Assuming your Swagger setup function dynamically sets the host based on the request
+    const expectedHost = `localhost:${server.address().port}`;
+    expect(swaggerDoc.host).toBe(expectedHost);
+  });
+});
